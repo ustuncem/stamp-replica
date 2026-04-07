264bef8a1d282548e19bac622817c0870be8cc90
ustuncem <ustuncem7@gmail.com>
2026-04-07 14:57:53 +0300

    feat(router): first try

---
+++ src/app/galleries/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Galleries() {
+	return (
+		<View>
+			<Text>Galleries</Text>
+		</View>
+	);
+}
---
+++ src/app/stamper/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Stamper() {
+	return (
+		<View>
+			<Text>Stamper</Text>
+		</View>
+	);
+}
